fded661385ee91a0ffd0b4e601e160822d41b762
NasserRamadan <123755983+NasserRamadan@users.noreply.github.com>
2023-12-11 10:26:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-$('form').on('submit', function(e) {
-    e.preventDefault();
+$(document).ready(function(){
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+
+        const novaTarefa = $('#nova-tarefa').val();
+        const novoItem =$('<li></li>').text(novaTarefa);
 
 
-    const novaTarefa = $('#nova-tarefa').val();
-    const novoItem =$('<li></li>');
+        $(`${novaTarefa}`).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $('#nova-tarefa').val('')
 
 
-    $(`<li>${novaTarefa}</li>`).appendTo(novoItem);
-    $(`<div class="tasks-list" />
-        <a href="${novaTarefa}"></a>
-    </div>`)
+        $(`<div class="tasks-list" />
+            <a href="${novaTarefa}"></a>
+        </div>`)
 
-
-    $('#nova-tarefa').val('')
+    });
 });
